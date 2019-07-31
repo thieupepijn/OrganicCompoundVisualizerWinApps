7472f17661f21862370ecc33ad6737351e33b7ae
thieupepijn <thieupepijn@hotmail.com>
2019-07-31 22:27:38 +0200

    eleventh commit

--- OrganicCompoundVisualizerWPF/Window1.xaml.cs
+++ OrganicCompoundVisualizerWPF/Window1.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows;
 using System.Linq;
 using IUPAC2Image;
 
+
 namespace OrganicCompoundVisualizerWPF
 {
 	/// <summary>
@@ -24,7 +25,11 @@ namespace OrganicCompoundVisualizerWPF
 		public Window1()
 		{
 			InitializeComponent();
+			InitializeColorComboBoxen();
+		
 		}
+		
+		
 		void BtnDraw_Click(object sender, RoutedEventArgs e)
 		{
 			
@@ -35,7 +40,12 @@ namespace OrganicCompoundVisualizerWPF
 			
 			try
 			{
-				IUPAC2ImageConverter converter = new IUPAC2ImageConverter(iupacname, width, height, 15, 100, 2, Color.Gray, Brushes.Black, Brushes.Orange, Brushes.DarkBlue);
+				Color backgroundColor = GetSelectedColor(cmbBackGroundColor);
+				Brush fontBrush = GetSelectedColorAsBrush(cmbFontColor);
+				Brush nodeBrush = GetSelectedColorAsBrush(cmbNodeColor);
+				Brush verticeBrush = GetSelectedColorAsBrush(cmbVerticeColor);
+				
+				IUPAC2ImageConverter converter = new IUPAC2ImageConverter(iupacname, width, height, 15, 100, 2, backgroundColor, fontBrush, nodeBrush, verticeBrush);
 				imgIUPAC.Source = converter.DrawToBitmapImage();
 			}
 			catch(Exception exception)
@@ -46,7 +56,43 @@ namespace OrganicCompoundVisualizerWPF
 		}
 		
 		
+		private void InitializeColorComboBoxen()
+		{
+		  cmbBackGroundColor.ItemsSource = typeof(Color).GetProperties();
+		  cmbBackGroundColor.DisplayMemberPath = "Name";
+		  cmbBackGroundColor.SelectedValuePath = "Name";
+		  cmbBackGroundColor.SelectedIndex = 1;	
+
+		  cmbFontColor.ItemsSource = typeof(Color).GetProperties();
+		  cmbFontColor.DisplayMemberPath = "Name";
+		  cmbFontColor.SelectedValuePath = "Name";
+		  cmbFontColor.SelectedIndex = 1;	
+		  
+		  cmbNodeColor.ItemsSource = typeof(Color).GetProperties();
+		  cmbNodeColor.DisplayMemberPath = "Name";
+		  cmbNodeColor.SelectedValuePath = "Name";
+		  cmbNodeColor.SelectedIndex = 1;
+		  
+		  cmbVerticeColor.ItemsSource = typeof(Color).GetProperties();
+		  cmbVerticeColor.DisplayMemberPath = "Name";
+		  cmbVerticeColor.SelectedValuePath = "Name";
+		  cmbVerticeColor.SelectedIndex = 1;
+		  
+		  
+		}
 		
+		
+		private Color GetSelectedColor(System.Windows.Controls.ComboBox combobox)
+		{
+			string colorName = combobox.SelectedValue.ToString();
+			return (Color)new ColorConverter().ConvertFromString(colorName);
+		}
+		
+		private Brush GetSelectedColorAsBrush(System.Windows.Controls.ComboBox combobox)
+		{
+			Color color = GetSelectedColor(combobox);
+			return new SolidBrush(color);
+		}
 		
 	}
 }
